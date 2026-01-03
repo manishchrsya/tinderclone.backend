8e45c195145951b4540d48fdf10ea1527f0636fc
Manish Chaurasiya <chaurasiya.mns@gmail.com>
2026-01-04 01:32:15 +0530

    few more changes made

--- src/app.js
+++ src/app.js
@@ -2,16 +2,27 @@ const express = require("express");
 
 const app = express();
 
+app.use("/", (req, res, next) => {
+    console.log("home page");
+    // res.send("home page");
+    next();
+});
+
 app.use("/user",
     (req, res, next) => {
         next();
         console.log("handler 1");
-        // res.send("users list");
-    }, (req, res, next) => {
-        console.log("handler 2");
-        res.send("user created");
-        next();
+        res.send("users list");
+        // next();
     });
+
+app.use("/user",
+    (req, res, next) => {
+        next();
+        console.log("handler 2");
+        res.send("users list 1");
+    });
+
 app.listen(3000, () => {
     console.log("server started of the tinder on port 3000...");
 });
