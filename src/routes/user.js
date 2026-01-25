afdb5548d45ef47f050de5adbe25f4efbcd65f14
Manish Chaurasiya <chaurasiya.mns@gmail.com>
2026-01-26 00:44:13 +0530

    feed api created

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const { userAuth } = require("../middlewares/auth");
 const connectionRequestModel = require("../models/connectRequests");
+const { UserModel } = require("../models/user");
 
 const router = Router();
 
@@ -45,6 +46,51 @@ router.get("/user/connections", userAuth, async (req, res) => {
         res.status(400).send(error.message || "Something went wrong");
     }
 
+});
+
+router.get("/user/feed", userAuth, async (req, res) => {
+    try {
+        /**
+         * 1. loggedin user should not see his profile in the feed
+         * 2. loggedin users should not the profiles he is in a connection with
+         * 3. loggedin user should not see the profiles whom he has send 
+         *    request/ignored or is rejected earlier or been done similar by other profile
+         * 
+         * */
+        const loggedInUser = req.user;
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1).limit;
+
+
+        const requests = await connectionRequestModel.find({
+            $or: [
+                { toUserId: loggedInUser._id },
+                { fromUserId: loggedInUser._id }
+            ]
+        }).select("fromUserId toUserId");
+        // .populate("fromUserId", "firstName").populate("toUserId", "firstName");
+        // console.log("requests", requests);
+        // console.log("request", requests, requests.length);
+        const hideUsersFromFeed = new Set();
+        requests.forEach((request) => {
+            hideUsersFromFeed.add(request.fromUserId);
+            hideUsersFromFeed.add(request.toUserId);
+        });
+
+        const users = await UserModel.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUsersFromFeed) } },
+                { _id: { $ne: loggedInUser._id } }
+            ]
+        }).select(["firstName", "lastName", "photoUrl", "about", "age", "skills"]).skip(skip).limit(limit);
+
+        res.send({ message: "success", data: users });
+    } catch (error) {
+        console.log("error", error);
+
+    }
 });
 
 
